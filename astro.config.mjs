fd99cabce2e05b50fb038490a6c6b550cec521a9
Taylor Beseda <tbeseda@gmail.com>
2022-04-05 11:45:33 -0600

    add ssr with node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import nodejs from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: nodejs(),
+});
